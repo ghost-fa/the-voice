49a60ffcef297da7b78edfa23c0f4a0d9866cbe1
ghost-fk <fayyad_r22@yahoo.com>
2018-09-10 12:04:37 +0200

    select the text earey

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
-const msg = new speechSynthesisUtterance();
+const msg = new SpeechSynthesisUtterance();
 
 let voices = [];
 const voicesDropdown = document.querySelector(`[name="voice"]`);
 const options = document.querySelectorAll(`[type="range"],[name="text"]`);
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+
+msg.text = document.querySelector(`[name="text"]`).value;
